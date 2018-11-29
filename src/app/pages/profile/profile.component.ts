8b153514ec4c81356985f5281bea5316ce813c90
Pham Quang Huy <p.q.huy97@gmail.com>
2018-11-29 21:12:15 +0700

    booking history and changing password site

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -7,10 +7,12 @@ declare var $: any;
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  private type: Number;
 
   constructor() { }
 
   ngOnInit() {
+    this.type = 1;
   }
 
   ngAfterViewInit() {
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -4,9 +4,11 @@ import { ProfileComponent } from './profile.component';
 import { RouterModule } from '@angular/router';
 import { ProfileRoutes } from './profile.routes';
 import { CommonsModule } from '../../commons/commons.module';
+import { HistoryComponent } from './history/history.component';
+import { PasswordComponent } from './password/password.component';
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, HistoryComponent, PasswordComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(ProfileRoutes),
